8161b293d6bb7dd1e7b5f2759f60ada383acbc46
Denma Syarikin <denmasyarikin@gmail.com>
2018-01-07 12:20:27 +0700

    [UPDATE] add sales setting

---
+++ src/Setting.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Denmasyarikin\Sales;
+
+use App\Manager\Contracts\Setting as SettingContract;
+
+class Setting implements SettingContract
+{
+    /**
+     * get setting.
+     *
+     * @return array
+     */
+    public function getSetting()
+    {
+        return [
+            'material_icon' => 'receipt',
+        ];
+    }
+}
